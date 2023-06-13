4ad2a9fa238fd03d6d0fd3c501a09f7088c94b2d
Yuriy Gelvih <Gelvihurij@gmail.com>
2023-06-13 11:57:58 +0700

    Rework Hometask04

--- Seminar04/BinarySearchTree.java
+++ Seminar04/BinarySearchTree.java
@@ -2,45 +2,48 @@ package Seminar04;
 
 public class BinarySearchTree {
 
-    public class Tree<V extends Comparable<V>> {
+    public class Tree {
 
         private TreeNode root;
     
         private class TreeNode {
-            private V value;
+            private int value;
             private TreeNode left;
             private TreeNode right;            
         }
 
-        public boolean find(V value){
+        public boolean find(int value){
             TreeNode Treenode = root;
             while (Treenode != null){
-               if (Treenode.value.equals(value)){
-                  return true;
-               }
-               if (Treenode.value.compareTo(value) > 0) {
-                   Treenode = Treenode.left;
-                }else {
-                   Treenode = Treenode.right;
+                if (Treenode.value == value){
+                    return true;
+                }
+                else if (Treenode.value > value) {
+                    Treenode = Treenode.left;
+                }
+                else{
+                    Treenode = Treenode.right;
                 }
             }
          return false;
         }
 
-        public boolean recursionFind(V value) {
+        public boolean recursionFind(int value) {
             if (root == null){
                 return false;
             }
             return recursionFind(root, value);
         }
 
-        private boolean recursionFind(TreeNode Treenode, V value) {
-            if (Treenode.value.equals(value)){
+        private boolean recursionFind(TreeNode Treenode, int value) {
+            if (Treenode.value == value){
                 return true;
-            } else {
-                if (Treenode.value.compareTo(value) > 0){
+            } 
+            else {
+                if (Treenode.value > value){
                     return recursionFind(Treenode.left, value);
-                } else {
+                } 
+                else {
                     return recursionFind(Treenode.right, value);
                 }
             }
--- Seminar04/HomeTask.java
+++ Seminar04/HomeTask.java
@@ -1,128 +1,117 @@
 package Seminar04;
 
 public class HomeTask {
-    public class Tree<V extends Comparable<V>> {
-    private Node root;
+    public class Tree {
+        private Node root;
 
-    private class Node {
-        private V value;
-        
-        private Color color;
-        private Node leftChild;
-        private Node rightChild;
-    }
-    
-    private enum Color {
-        RED, BLACK
-    }
-
-    private Node rightChildSwap(Node node) {
-        Node rightChildChild = node.rightChild;
-        Node betweenChild = rightChildChild.leftChild;
-        rightChildChild.leftChild = node;
-        node.rightChild = betweenChild;
-        rightChildChild.color = node.color;
-        node.color = Color.RED;
-        return rightChildChild;
-    }
-
-    private Node leftChildSwap(Node node) {
-        Node leftChildChild = node.leftChild;
-        Node betweenChild = leftChildChild.rightChild;
-        leftChildChild.rightChild = node;
-        node.leftChild = betweenChild;
-        leftChildChild.color = node.color;
-        node.color = Color.RED;
-        return leftChildChild;
-    }
-
-    private void colorSwap(Node node) {
-        node.rightChild.color = Color.BLACK;
-        node.leftChild.color = Color.BLACK;
-        node.color = Color.RED;
-    }
-
-    public boolean add(V value) {
-        if (root != null) {
-            boolean result = addNode(root, value);
-            root = rebalance(root);
-            root.color = Color.BLACK;
-            return result;
-        } 
-        else {
-            root = new Node();
-            root.color = Color.BLACK;
-            root.value = value;
-            return true;
+        private class Node {
+            private int value;
+            
+            private Color color;
+            private Node leftChild;
+            private Node rightChild;
         }
-    }
-    private boolean addNode(Node node, V value) {
-        if (node.value == value) {
-            return false;
-        } else {
-            if (node.value.compareTo(value) > 0) {
-                if (node.leftChild != null) {
-                    boolean result = addNode(node.leftChild, value);
-                    node.leftChild = rebalance(node.leftChild);
-                    return result;
-                } 
-                else {
-                    node.leftChild = new Node();
-                    node.leftChild.color = Color.RED;
-                    node.leftChild.value = value;
-                    return true;
-                }
+        
+        private enum Color {
+            RED, BLACK
+        }
+
+        private Node rightChildSwap(Node node) {
+            Node rightChildChild = node.rightChild;
+            Node betweenChild = rightChildChild.leftChild;
+            rightChildChild.leftChild = node;
+            node.rightChild = betweenChild;
+            rightChildChild.color = node.color;
+            node.color = Color.RED;
+            return rightChildChild;
+        }
+
+        private Node leftChildSwap(Node node) {
+            Node leftChildChild = node.leftChild;
+            Node betweenChild = leftChildChild.rightChild;
+            leftChildChild.rightChild = node;
+            node.leftChild = betweenChild;
+            leftChildChild.color = node.color;
+            node.color = Color.RED;
+            return leftChildChild;
+        }
+
+        private void colorSwap(Node node) {
+            node.rightChild.color = Color.BLACK;
+            node.leftChild.color = Color.BLACK;
+            node.color = Color.RED;
+        }
+
+        public boolean add(int value) {
+            if (root != null) {
+                boolean result = addNode(root, value);
+                root = rebalance(root);
+                root.color = Color.BLACK;
+                return result;
             } 
             else {
-                if (node.rightChild != null) {
-                    boolean result = addNode(node.rightChild, value);
-                                        node.rightChild = rebalance(node.rightChild);
-                    return result;
+                root = new Node();
+                root.color = Color.BLACK;
+                root.value = value;
+                return true;
+            }
+        }
+        private boolean addNode(Node node, int value) {
+            if (node.value == value) {
+                return false;
+            } else {
+                if (node.value > value) {
+                    if (node.leftChild != null) {
+                        boolean result = addNode(node.leftChild, value);
+                        node.leftChild = rebalance(node.leftChild);
+                        return result;
+                    } 
+                    else {
+                        node.leftChild = new Node();
+                        node.leftChild.color = Color.RED;
+                        node.leftChild.value = value;
+                        return true;
+                    }
                 } 
                 else {
-                    node.rightChild = new Node();
-                    node.rightChild.color = Color.RED;
-                    node.rightChild.value = value;
-                    return true;
+                    if (node.rightChild != null) {
+                        boolean result = addNode(node.rightChild, value);
+                                            node.rightChild = rebalance(node.rightChild);
+                        return result;
+                    } 
+                    else {
+                        node.rightChild = new Node();
+                        node.rightChild.color = Color.RED;
+                        node.rightChild.value = value;
+                        return true;
+                    }
                 }
             }
         }
-    }
 
-    private Node rebalance(Node node) {
-        Node result = node;
-        boolean needRebalance;
-        do {
-            needRebalance = false;
-            if (result.rightChild != null && result.rightChild.color == Color.RED &&
-                    (result.leftChild == null || result.leftChild.color == Color.BLACK)) {
-                needRebalance = true;
-                result = rightChildSwap(result);
+        private Node rebalance(Node node) {
+            Node result = node;
+            boolean needRebalance;
+            do {
+                needRebalance = false;
+                if (result.rightChild != null && result.rightChild.color == Color.RED &&
+                        (result.leftChild == null || result.leftChild.color == Color.BLACK)) {
+                    needRebalance = true;
+                    result = rightChildSwap(result);
+                }
+                if (result.leftChild != null && result.leftChild.color == Color.RED &&
+                        result.leftChild.leftChild != null && result.leftChild.leftChild.color == Color.RED) {
+                    needRebalance = true;
+                    result = leftChildSwap(result);
+                }
+                if (result.leftChild != null && result.leftChild.color == Color.RED &&
+                        result.rightChild != null && result.rightChild.color == Color.RED) {
+                    needRebalance = true;
+                    colorSwap(result);
+                }
             }
-            if (result.leftChild != null && result.leftChild.color == Color.RED &&
-                    result.leftChild.leftChild != null && result.leftChild.leftChild.color == Color.RED) {
-                needRebalance = true;
-                result = leftChildSwap(result);
-            }
-            if (result.leftChild != null && result.leftChild.color == Color.RED &&
-                    result.rightChild != null && result.rightChild.color == Color.RED) {
-                needRebalance = true;
-                colorSwap(result);
-            }
+            while (needRebalance);
+            return result;
         }
-        while (needRebalance);
-        return result;
-    }
-
-    
-
-    
-
-    
-
-    
-}
-
-
-    
+    }    
 }
